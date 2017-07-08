e3d14d26e90d2dcc112a714d93fe70b40d3ab61a
Günther Foidl <gue@korporal.at>
2017-07-08 17:28:52 +0200

    Add project files.

---
+++ tests/Calculator.Core.Tests/CalculatorTests/Add.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace Calculator.Core.Tests.CalculatorTests
+{
+	[TestFixture]
+	public class Add
+	{
+		[Test]
+		public void Summands_given___correct_result()
+		{
+			// Arrange
+			int a = 3;
+			int b = 4;
+
+			var sut = new Calculator();
+
+			// Act:
+			int actual = sut.Add(a, b);
+
+			// Assert:
+			Assert.AreEqual(7, actual);
+		}
+	}
+}
