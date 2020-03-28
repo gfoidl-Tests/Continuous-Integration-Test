479d27832ca39ca2bb13590961afc49cf6ffe701
Günther Foidl <gue@korporal.at>
2020-03-28 11:56:57 +0100

    CI Update (#6)
    * Removed unneeded CI (bb-pipeline, appveyor, travis-ci), added gh-actions
    
    * api-doc
    
    * Azure Pipelines updated
    
    * xml doc comments
    
    * tabs -> spaces

---
+++ api-doc/index.md
@@ -0,0 +1,3 @@
+# Calculator
+
+Just a test / demo for CI-features.
--- source/Calculator.Core/Calculator.cs
+++ source/Calculator.Core/Calculator.cs
@@ -1,12 +1,27 @@
 ﻿namespace Calculator.Core
 {
+    /// <summary>
+    /// Simple calculator
+    /// </summary>
     public class Calculator
     {
+        /// <summary>
+        /// Addition
+        /// </summary>
+        /// <param name="a">First summand</param>
+        /// <param name="b">Second summand</param>
+        /// <returns>Sum of <paramref name="a"/> and <paramref name="b"/>.</returns>
         public int Add(int a, int b)
         {
             return a + b;
         }
         //---------------------------------------------------------------------
+        /// <summary>
+        /// Subtraction
+        /// </summary>
+        /// <param name="a">Minuend</param>
+        /// <param name="b">Subtrahend</param>
+        /// <returns>Difference of <paramref name="a"/> and <paramref name="b"/>.</returns>
         public int Subtract(int a, int b) => a - b;
     }
 }
